db8b8955a16462532cf464b4bddec0dfb2edfa5c
fegie8319 <fegie8319@gmail.com>
2021-01-25 23:06:44 +0800

    done

--- Beginner Ideas/Prime Finder.py
+++ Beginner Ideas/Prime Finder.py
@@ -1,33 +1,21 @@
 # # Code a script which finds the nth prime number for you. You just enter a number and it gives you the respective prime number. Example: Input an 11 and get a 31.
-#
-# # user_input = input("請輸入想找第幾個質數: ")
-# # cal = int(user_input)
-#
+
+user_input = input("請輸入想找第幾個質數: ")
+cal = int(user_input)
 init_num = 2
-prime_list = [2]
+listx = [2]
 
 while True:
-    i = 2
-    for i in range(2, init_num):
-        print(i)
-        if init_num % i == 0:
-            break
-        else:
-            prime_list.append(init_num)
-            break
-    if len(prime_list) == 10:
-        print(prime_list)
+    if len(listx) == cal:
+        ans = listx[-1]
+        print(ans)
         break
     init_num += 1
 
-# list1 = []
-# i = 2
-# for i in range(2,101):
-#     j = 2
-#     for j in range (2,i):
-#         if i%j == 0:
-#             break
-#     else:
-#         list1.append(i)
-#
-# print(list1)
+    for i in range(2, init_num):
+        if init_num % i == 0:
+            break
+
+        if i == init_num -1:
+            listx.append(init_num)
+            break
