25f59c8116b6481bc0770ca2f7535d7ae7b4e82c
fegie8319 <fegie8319@gmail.com>
2021-01-31 22:38:44 +0800

    add try attempt

--- Beginner Ideas/Number Guessing Game.py
+++ Beginner Ideas/Number Guessing Game.py
@@ -1,17 +1,21 @@
 # Develop a number guessing game. The computer comes up with a random number and you can make guesses. It then tells you if the secret number is greater or smaller. At the end it tells you how many attempts you needed.
 import random
 
-com_num = random.randint(1,101)
+com_num = random.randint(1, 101)
+attempt = 1
 
 while True:
     user_input = input("Please enter an integer from 1 to 100: ")
     user_in = int(user_input)
     if user_in < com_num:
+        attempt +=1
         print("It's smaller than the answer!")
 
     elif user_in > com_num:
+        attempt += 1
         print("It's bigger than the answer!")
 
     else:
         print("Congratulations!")
+        print(f"You tried {attempt} times!")
         break
